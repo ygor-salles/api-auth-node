96d0dc3ef9cb9580802bfabbd1fd6df1038834e5
unknown <ygorsallesaniceto@gmail.com>
2022-02-17 11:23:48 -0300

    add: crud user

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,18 @@
 import { Request, Response, Router } from 'express';
+import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Hellow world' });
 });
+
+const userController = new UserController();
+
+router.post('/users', userController.create);
+router.get('/users', userController.read);
+router.get('/users/:id', userController.readById);
+router.delete('/users/:id', userController.deleteById);
+router.put('/users/:id', userController.updateById);
 
 export { router };
